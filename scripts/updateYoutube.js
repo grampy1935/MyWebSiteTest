e2ec994112be792a81572621a3b4ce8f284cab75
ota takatoshi <grampy1935@yahoo.co.jp>
2025-10-02 10:39:53 +0900

    updateYoutube.jsとcustom.cssを編集した

--- scripts/updateYoutube.js
+++ scripts/updateYoutube.js
@@ -55,18 +55,18 @@ async function fetchAll() {
 function generateMarkdown(videos) {
   return videos.map(v => `
 ### ${v.title}
-
-<iframe
-  width="560"
-  height="315"
-  src="https://www.youtube.com/embed/${v.id}"
-  allowfullscreen
-  title="${v.title}"
-  frameborder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-  allowfullscreen>
-</iframe>
-
+<div class="video-container">
+    <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/${v.id}"
+    allowfullscreen
+    title="${v.title}"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+    allowfullscreen>
+    </iframe>
+</div>
 ${v.description || ''}
 `).join('\n');
 }
