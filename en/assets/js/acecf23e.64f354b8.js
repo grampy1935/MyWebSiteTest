4a8b19c3d7a1bafa8facef1533276fbbeaefa80f
grampy1935 <grampy1935@users.noreply.github.com>
2025-10-04 21:25:48 +0000

    deploy: b529ac54d517645e700a8e96e6dfb205e983bf93

---
+++ en/assets/js/acecf23e.64f354b8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/MyWebSiteTest/en/blog","blogTitle":"Blog","authorsListPath":"/MyWebSiteTest/en/blog/authors"}')}}]);
--- en/assets/js/acecf23e.f6afbae0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/MyWebSite/en/blog","blogTitle":"Blog","authorsListPath":"/MyWebSite/en/blog/authors"}')}}]);
